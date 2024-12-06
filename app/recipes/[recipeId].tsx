2e60c98ddddaf32d265fe4511958bfe7315afeb2
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2024-12-05 21:21:35 -0500

    new fonts

--- app/recipes/[recipeId].tsx
+++ app/recipes/[recipeId].tsx
@@ -24,6 +24,8 @@ export default function RecipePage() {
 
 	useEffect(() => {
 		Font.loadAsync({
+		'Neuton Bold': require('../../assets/fonts/Neuton-Bold.ttf'),
+		'Merriweather Sans': require('../../assets/fonts/MerriweatherSans.ttf'),
 		'Crete Round': require('../../assets/fonts/CreteRound-Regular.ttf'),
 		'Inter': require('../../assets/fonts/Inter-Regular.ttf'),
 		}).then(() => setFontsLoaded(true));
@@ -47,7 +49,6 @@ export default function RecipePage() {
 						</TouchableOpacity>
 					</View>
 					{/* Title */}
-					<Text style={styles.title}>Rice & Beef Skillet</Text>
 
 					{/* Image with shadow */}
 					<Image
@@ -56,6 +57,7 @@ export default function RecipePage() {
 					/>
 
 					{/* Recipe details */}
+					<Text style={styles.title}>Rice & Beef Skillet</Text>
 					<Text style={styles.details}>
 						60min • 500 cal/serving • 3 servings
 					</Text>
@@ -64,8 +66,11 @@ export default function RecipePage() {
 					
 					{/* <Text>Recipe ID: {recipeId}</Text> */}
 
+
 					{/* Ingredients List */}
 					<View style={styles.section}>
+					<Text style={styles.sectionHeader}>Ingredients</Text>
+
 						<Text style={styles.ingredient}>• 1 onion, chopped</Text>
 						<Text style={styles.ingredient}>• 1 lb ground beef</Text>
 						<Text style={styles.ingredient}>• 1 cup monterey jack cheese</Text>
@@ -84,6 +89,8 @@ export default function RecipePage() {
 
 					{/* Recipe Steps */}
 					<View style={styles.section}>
+					
+					<Text style={styles.sectionHeader}>Instructions</Text>
 						<Text style={styles.step}>
 							1. Heat olive oil in a skillet and{" "}
 							<Text
@@ -116,30 +123,30 @@ const styles = StyleSheet.create({
 		backgroundColor: "#FAF3ED",
 	},
 	contentContainer: {
-		padding: 32,
+		// padding: 32,
 		paddingTop: 0,
 	},
 	headerContainer: {
-		padding: 16,
+		// padding: 16,
 		paddingBottom: 0,
 	},
 	header: {
 		flexDirection: "row",
 		justifyContent: "space-between",
 		alignItems: "center",
-		marginBottom: 16,
+		// marginBottom: 16,
 	},
 	title: {
-		fontSize: 32,
-		fontFamily: "Crete Round",
+		fontSize: 43,
+		fontFamily: "Neuton Bold",
 		color: "black",
-		marginBottom: 8,
+		// marginBottom: 8,
 	},
 	image: {
 		width: "100%",
 		height: 200,
 		borderRadius: 8,
-		marginBottom: 16,
+		// marginBottom: 16,
 		shadowColor: "#000000",
 		shadowOffset: { width: 0, height: 4 },
 		shadowOpacity: 0.3,
@@ -147,18 +154,24 @@ const styles = StyleSheet.create({
 	},
 	details: {
 		fontSize: 16,
-		fontFamily: "Crete Round", // Make sure this font is loaded in your project
+		fontFamily: "Merriweather Sans", // Make sure this font is loaded in your project
 		color: "black",
-		marginBottom: 16,
+		// marginBottom: 16,
 	},
 	section: {
-		borderTopWidth: 1,
-		borderColor: "#A9A9B0",
-		paddingVertical: 16,
+		// borderTopWidth: 1,
+		// borderColor: "#A9A9B0",
+		// paddingVertical: 16,
+	},
+	sectionHeader: {
+		fontSize: 24,
+		fontFamily: "Neuton Bold",
+		color: "black",
+		marginBottom: 4,
 	},
 	ingredient: {
 		fontSize: 16,
-		fontFamily: "Crete Round",
+		fontFamily: "Merriweather Sans",
 		color: "black",
 		marginBottom: 4,
 	},
@@ -172,12 +185,12 @@ const styles = StyleSheet.create({
 	},
 	buttonText: {
 		color: "white",
-		fontFamily: "Crete Round",
+		fontFamily: "Merriweather Sans",
 		fontSize: 16,
 	},
 	step: {
 		fontSize: 16,
-		fontFamily: "Crete Round",
+		fontFamily: "Merriweather Sans",
 		color: "black",
 		marginBottom: 8,
 	},
